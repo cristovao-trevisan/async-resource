3947e6b2a875dc684480d98c68eda2a7243bcf79
Cristóvão Trevisan <cristovao@contabilizei.com.br>
2018-08-22 17:42:56 -0300

    Implementing cache logic

--- src/storage/index.native.ts
+++ src/storage/index.native.ts
@@ -2,17 +2,7 @@ import { AsyncStorage } from 'react-native'
 
 import { GenericStorage } from './types'
 
-const hasLocalStorage = () => {
-  try {
-    localStorage.setItem('hasLocalStorage', 'test')
-    localStorage.removeItem('hasLocalStorage')
-    return true
-  } catch (e) {
-    return false
-  }
-}
-
-export class NativeStorage implements GenericStorage {
+class NativeStorage implements GenericStorage {
   set = (key: string, value: any) => AsyncStorage.setItem(key, JSON.stringify(value))
   get = async (key: string) => {
     const item = await AsyncStorage.getItem(key)
@@ -21,6 +11,8 @@ export class NativeStorage implements GenericStorage {
       : null
   }
   remove = (key: string) => AsyncStorage.removeItem(key)
+
+  clear = () => AsyncStorage.clear()
 }
 
 export default new NativeStorage()
--- src/storage/types.ts
+++ src/storage/types.ts
@@ -2,4 +2,5 @@ export interface GenericStorage {
   set: (key: string, value: any) => Promise<void>
   get: (key: string) => Promise<any | null>
   remove: (key: string) => Promise<void>
+  clear: () => Promise<void>
 }
